199ec52ae961481a5d1e2a597a61f4913fdb9f28
Mento79 <bodyomar79@gmail.com>
2022-04-20 23:00:59 +0200

    push nowww

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,11 +3,23 @@ import java.util.*;
 
 public class Main {
 
+    public static List<Integer> root = new ArrayList<>();
+    public static HashMap<Integer, List<Integer>> heap = new HashMap<>();
+    public static HashMap<Integer, Integer> temp = new HashMap<>();
+    public static HashMap<Integer, Integer> usedObject = new HashMap<>();
+    public static int starting;
+    public static void compact(int x){
+        int cost=heap.get(x).get(1)-heap.get(x).get(0);
+        heap.get(x).clear();
+        heap.get(x).add(starting);
+        starting+=cost;
+        heap.get(x).add(starting);
+    }
     public static void main(String[] args) throws IOException {
+        starting=-1;
         ////////ROOT///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         File myObj = new File("root.txt");
         Scanner myReader = new Scanner(myObj);
-        List<Integer> root = new ArrayList<>();
         while (myReader.hasNextLine()) {
             String data = myReader.nextLine();
             root.add(Integer.parseInt(data));
@@ -16,10 +28,10 @@ public class Main {
         /////////Heap//////////////////////////////////////////
         File myObj2 = new File("heap.txt");
         Scanner myReader2 = new Scanner(myObj2);
-        HashMap<Integer, List<Integer>> heap = new HashMap<>();
         while (myReader2.hasNextLine()) {
             String data = myReader2.nextLine();
             String[] res = data.split(",", 0);
+            starting=starting==-1? Integer.parseInt(res[1]) :starting;
             heap.put(Integer.parseInt(res[0]), new ArrayList<>());
             for (int i = 1; i < res.length; i++) {
                 heap.get(Integer.parseInt(res[0])).add(Integer.parseInt(res[i]));
@@ -29,7 +41,6 @@ public class Main {
         /////////pointers//////////////////////////////////////////
         File myObj3 = new File("pointers.txt");
         Scanner myReader3 = new Scanner(myObj3);
-        HashMap<Integer, Integer> temp = new HashMap<>();
         while (myReader3.hasNextLine()) {
             String data = myReader3.nextLine();
             String[] res = data.split(",", 0);
@@ -37,7 +48,6 @@ public class Main {
         }
 
 
-        HashMap<Integer, Integer> usedObject = new HashMap<>();
         for (Integer integer1 : root) {
             for (Integer integer : temp.keySet()) {
                 int key = integer;
@@ -78,6 +88,8 @@ public class Main {
             Mark.put(item, false);
         }
 
+
+        /*
         for (Integer i : root) {
             Mark.put(i, true);
             int x = i;
@@ -89,11 +101,52 @@ public class Main {
             }
         }
 
+        */
+        boolean first=false;
+        for (Integer i : root) {
+            Mark.put(i,true);
+            compact(i);
+            int x=i;
+            while(usedObject.containsKey(x)){
+                x=usedObject.get(x);
+                System.out.println("\nchild " + x);
+                if(Mark.get(x))break;
+                Mark.put(x,true);
+                compact(x);
+
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         for (Integer item : Mark.keySet()) {
             if (!Mark.get(item)) {
                 heap.remove(item);
             }
         }
+
+
+
+
+
+
+
+
+
+
+
 
         try {
             File markAndSweepFile = new File("out1.txt");
